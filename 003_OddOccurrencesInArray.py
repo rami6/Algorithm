4eab9d4f4846028019ce173c981f367c72b415db
rami6 <30137645+rami6@users.noreply.github.com>
2018-09-17 22:22:50 -0700

    Add an answer

---
+++ 003_OddOccurrencesInArray.py
@@ -0,0 +1,17 @@
+"""
+Problem description
+ - https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+
+score
+ - 100%
+"""
+
+
+from collections import Counter
+
+
+def solution(A):
+    c = Counter(A)
+    for key, val in c.items():
+        if val % 2 != 0:
+            return key
