70527545492325375efbd010a03e063998f533d5
rami6 <30137645+rami6@users.noreply.github.com>
2018-09-17 22:09:04 -0700

    Add an answer

---
+++ 002_CyclicRotation.py
@@ -0,0 +1,23 @@
+"""
+Problem description
+ - https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+
+score
+ - 100%
+"""
+
+
+def solution(A, K):
+    if len(A) == 0:
+        return A
+
+    new_arr = []
+    prev_arr = A
+    for i in range(K):
+        new_arr.append(prev_arr[-1])
+        for j in range(len(A) - 1):
+            new_arr.append(prev_arr[j])
+        prev_arr = new_arr
+        new_arr = []
+
+    return prev_arr
