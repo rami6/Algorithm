72f699dc8df3cffec600070435e75bed57176d27
rami6 <30137645+rami6@users.noreply.github.com>
2018-09-17 18:42:06 -0700

    Initial commit

---
+++ 01_BinaryGap.py
@@ -0,0 +1,30 @@
+"""
+Problem description
+ - https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+
+score
+ - 100%
+"""
+
+
+def solution(N):
+    bin_N = bin(N)
+    str_bin_N = str(bin_N)[2:]
+
+    zero_max = 0
+    one_inds = []
+
+    i = 0
+    while i < len(str_bin_N):
+        ind = str_bin_N.find('1', i)
+        if ind >= 0:
+            one_inds.append(ind)
+            i = ind + 1
+        else:
+            break
+
+    for i in range(1, len(one_inds)):
+        zeros = one_inds[i] - one_inds[i - 1] - 1
+        zero_max = max(zero_max, zeros)
+
+    return zero_max
