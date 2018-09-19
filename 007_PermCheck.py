42e2f081dba7ae0da3ee8210374fe32138809227
rami6 <30137645+rami6@users.noreply.github.com>
2018-09-19 00:55:16 -0700

    Add 007_PermCheck

---
+++ 007_PermCheck.py
@@ -0,0 +1,18 @@
+"""
+Problem description
+ - https://app.codility.com/programmers/lessons/4-counting_elements/perm_check/
+
+score
+ - 100%
+"""
+
+from collections import Counter
+
+
+def solution(A):
+    c = Counter(A)
+    for i in range(1, len(A) + 1):
+        if c[i] != 1:
+            return 0
+
+    return 1
