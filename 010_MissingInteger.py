61f25a6fb542d6f6bd2177e7e9371cc3b0a459e4
rami6 <30137645+rami6@users.noreply.github.com>
2018-09-25 16:05:05 -0700

    Improve the answer

--- 010_MissingInteger.py
+++ 010_MissingInteger.py
@@ -3,16 +3,26 @@ Problem description
  - https://app.codility.com/programmers/lessons/4-counting_elements/missing_integer/
 
 score
- - 66%
+ - 100%
 """
 
-# Correct, but poor efficiency
 
 def solution(A):
-    i = 1
-    while i <= max(A):
-        if not i in A:
-            return i
+    # Check if 1 should be returned.
+    if 1 not in A:
+        return 1
+
+    # Check the case the length of A is 1 and the condition above is not applicable.
+    if A == [1]:
+        return 2
+
+    # Check other cases.
+    A.sort()
+
+    i = 0
+    while i < len(A) - 1:
+        if A[i] >= 0 and A[i + 1] - A[i] > 1:
+            return A[i] + 1
         i += 1
 
-    return i
+    return A[len(A) - 1] + 1
