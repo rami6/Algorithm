319a0df117ea88f94573249d3e09403cbc251baa
rami6 <30137645+rami6@users.noreply.github.com>
2018-09-26 11:56:34 -0700

    Add 013_GenomicRangeQuery

---
+++ 013_GenomicRangeQuery .py
@@ -0,0 +1,25 @@
+"""
+Problem description
+ - https://app.codility.com/programmers/lessons/5-prefix_sums/genomic_range_query/
+
+score
+ - 62% ...Performance is good, but less correct
+"""
+
+
+def solution(S, P, Q):
+    d = {'A': 1, 'C': 2, 'G': 3, 'T': 4}
+
+    minarr = []
+    minimum = 4
+
+    for i in range(len(P)):
+        for key in d.keys():
+            if S.find(key, P[i], Q[i] + 1) > 0:
+                minimum = min(minimum, d[key])
+                if minimum == 1:
+                    break
+        minarr.append(minimum)
+        minimum = 4
+
+    return minarr
