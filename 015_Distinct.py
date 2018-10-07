6b4d11256d76ea094d593859e485f36bbdc8e4ac
rami6 <30137645+rami6@users.noreply.github.com>
2018-10-06 23:28:19 -0700

    Add 015_Distinct

---
+++ 015_Distinct.py
@@ -0,0 +1,20 @@
+"""
+Problem description
+ - https://app.codility.com/programmers/lessons/6-sorting/distinct/
+
+score
+ - 100%
+"""
+
+
+def solution(A):
+    if len(A) == 0:
+        return 0
+
+    count = 1
+    A.sort()
+    for i in range(1, len(A)):
+        if A[i] != A[i - 1]:
+            count += 1
+
+    return count
