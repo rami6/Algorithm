525fa15da0bce4d5cf9b859c08751caaa641e0c8
rami6 <30137645+rami6@users.noreply.github.com>
2018-11-25 19:38:17 -0800

    Improve the answer

--- 016_Triangle.py
+++ 016_Triangle.py
@@ -6,17 +6,18 @@ Section
  - Sorting
 
 score
- - 81%: timeout error for input A of large negative values
+ - 100%
 """
 
 
 def solution(A):
     A.sort()
     for i in range(0, len(A)):
-        for j in range(i + 1, len(A)):
-            for k in range(j + 1, len(A)):
-                if A[i] + A[j] > A[k]:
-                    return 1
-                else:
-                    break
+        if A[i] > 0:
+            for j in range(i + 1, len(A)):
+                for k in range(j + 1, len(A)):
+                    if A[i] + A[j] > A[k]:
+                        return 1
+                    else:
+                        break
     return 0
