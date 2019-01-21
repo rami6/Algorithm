118c0be3e602cabdc55d392782ff4d593e98afd4
rami6 <30137645+rami6@users.noreply.github.com>
2019-01-20 22:39:18 -0800

    Add 0205_IsomorphicStrings

---
+++ 02_LeetCode/0205_IsomorphicStrings.py
@@ -0,0 +1,31 @@
+"""
+Problem description
+ - https://leetcode.com/problems/isomorphic-strings/
+
+Level
+ - Easy
+
+Result
+ - Runtime: 56 ms, faster than 98.72% of Python3 online submissions for Isomorphic Strings.
+"""
+
+
+class Solution:
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        char_dict = {}
+
+        for s_char, t_char in zip(s, t):
+            if s_char not in char_dict:
+                if t_char not in char_dict.values():
+                    char_dict[s_char] = t_char
+                else:
+                    return False
+            elif char_dict[s_char] != t_char:
+                return False
+
+        return True
