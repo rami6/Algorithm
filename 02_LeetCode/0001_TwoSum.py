298e83a8b4f146540046ada5c5c13e264a5e85ab
rami6 <30137645+rami6@users.noreply.github.com>
2019-02-03 13:26:07 -0800

    Improve answer

--- 02_LeetCode/0001_TwoSum.py
+++ 02_LeetCode/0001_TwoSum.py
@@ -3,8 +3,8 @@ Problem description
  - https://leetcode.com/problems/two-sum/
 
 Result
- - Runtime: 816 ms, faster than 40.95% of Python3 online submissions for Two Sum.
- - Memory Usage: 7.3 MB, less than 71.72% of Python3 online submissions for Two Sum.
+ - Runtime: 36 ms, faster than 99.75% of Python3 online submissions for Two Sum.
+ - Memory Usage: 7.9 MB, less than 31.08% of Python3 online submissions for Two Sum.
 """
 
 
@@ -16,9 +16,12 @@ class Solution:
         :rtype: List[int]
         """
 
+        index_dict = {}
+
         for i in range(len(nums)):
             complement = target - nums[i]
-            try:
-                return [i, nums[i + 1:].index(complement) + i + 1]
-            except:
-                pass
+            if complement in index_dict:
+                return [i, index_dict[complement]]
+
+            if nums[i] not in index_dict:
+                index_dict[nums[i]] = i
